c49ca5c325bc5fe75770c5bed44ab9651112756e
Costantino14 <costantino.grabesu14@gmail.com>
2024-04-12 15:43:20 +0200

    modifiche hover e lowerCase js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let locazione = document.getElementById("location");
 
 document.querySelector(".ricerca").addEventListener("click", function() {
-    if (locazione.value === "genova") {
+    if (locazione.value.toLowerCase() === "genova") {
         window.open("index2.html")
     } else { 
-        if (locazione.value === "inverness") {
+        if (locazione.value.toLowerCase() === "inverness") {
             window.open("index4.html")
         } else {
             alert("Devi digitare una location valida");
